41005e8ab0b061a93ccbd8be45e73d953c179492
Manoj Kumar AVM <manojavm@codeaurora.org>
2014-04-29 22:08:51 -0700

    hqd: Fix Klocwork errors in liblight
    1. Fix null pointer de-reference klocwork error in liblight.
    2. sprintf is part of ScanBan function. Replace sprintf with
       snprintf.
    
    Change-Id: I0b88967a2bdf4adf563753c09df88d2c6a432045

--- liblight/lights.c
+++ liblight/lights.c
@@ -55,7 +55,7 @@ write_int(char const* path, int value)
     fd = open(path, O_RDWR);
     if (fd >= 0) {
         char buffer[20];
-        int bytes = sprintf(buffer, "%d\n", value);
+        int bytes = snprintf(buffer, sizeof(buffer), "%d\n", value);
         int amt = write(fd, buffer, bytes);
         close(fd);
         return amt == -1 ? -errno : 0;
@@ -183,6 +183,9 @@ static int open_lights(const struct hw_module_t* module, char const* name,
 
     struct light_device_t *dev = malloc(sizeof(struct light_device_t));
     memset(dev, 0, sizeof(*dev));
+
+    if(!dev)
+        return -ENOMEM;
 
     dev->common.tag = HARDWARE_DEVICE_TAG;
     dev->common.version = 0;
