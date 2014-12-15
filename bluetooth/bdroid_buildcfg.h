5ffeaa8e86294c97b8dc26fb01b5e94d4b8be71d
Christopher R. Palmer <crpalmer@gmail.com>
2014-12-14 20:52:59 -0500

    victara: bluetooth: Update the bdroid config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,8 +18,5 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Moto X"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QCOM_SW TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
-#define BTC_INCLUDED TRUE
+
 #endif
