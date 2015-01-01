b700ef0007f25c78c8d9d0c31b5cc35801f0f49d
Christopher R. Palmer <crpalmer@gmail.com>
2015-01-01 06:34:37 -0500

    victara: Set EGL cache sizes
    Change-Id: Icca4b36f9a53629495da1e313927e65759a4e07c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,6 +79,16 @@ TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
 
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
 
