438d3342b37cc7b1944e386747367dc9b25e15a3
Lorenzo Colitti <lorenzo@google.com>
2015-03-05 16:05:23 +0900

    Remove the Ethernet configuration
    This is no longer necessary now that the Ethernet service is
    started on all devices that support USB OTG.
    
    Change-Id: Ib8f8265eab29eef0eca54e3fe2c7cc7b8f88966b

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
