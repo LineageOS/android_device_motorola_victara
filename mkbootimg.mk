06f9604dc5bd3d55688d55da629a769b9f5dd1d0
Christopher R. Palmer <crpalmer@gmail.com>
2015-03-16 00:23:49 +0000

    Revert "victara: boot.img: Compress the dt.img using lz4"
    This reverts commit c5796b532c7d5331742dd5d89bf4e24c2473189e.
    
    I guess we'll have to wait to get this support added.
    
    Change-Id: I4d09be16a17b461ca9aa4617d21e9e40d966ae43

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -16,18 +16,14 @@ endef
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
-RAW_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img.raw
 
-$(RAW_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
 	$(call append-cm-dtb)
-	$(call pretty,"Target raw dt image: $(RAW_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -2 -o $(RAW_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
+	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
-$(INSTALLED_DTIMAGE_TARGET): $(RAW_DTIMAGE_TARGET)
-	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	lz4 -9 < $(RAW_DTIMAGE_TARGET) > $@
 
 ## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
