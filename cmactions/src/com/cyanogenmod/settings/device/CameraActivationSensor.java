1098d26b36de528090d92d5dca7846a10b159842
Christopher R. Palmer <crpalmer@gmail.com>
2015-04-20 13:30:08 -0400

    victara: cmactions: Make the settings only store settings
    Don't use it to trigger actions.
    
    Change-Id: Ifc3c722117791bdb27a9fb5698fc7059369d6c44

--- cmactions/src/com/cyanogenmod/settings/device/CameraActivationSensor.java
+++ cmactions/src/com/cyanogenmod/settings/device/CameraActivationSensor.java
@@ -30,15 +30,19 @@ public class CameraActivationSensor implements SensorEventListener, UpdatedState
     private static final int TURN_SCREEN_ON_WAKE_LOCK_MS = 500;
 
     private final CMActionsSettings mCMActionsSettings;
+    private final SensorAction mAction;
     private final SensorHelper mSensorHelper;
 
     private final Sensor mSensor;
 
     private boolean mIsEnabled;
 
-    public CameraActivationSensor(CMActionsSettings cmActionsSettings, SensorHelper sensorHelper) {
+    public CameraActivationSensor(CMActionsSettings cmActionsSettings, SensorAction action,
+        SensorHelper sensorHelper) {
         mCMActionsSettings = cmActionsSettings;
+        mAction = action;
         mSensorHelper = sensorHelper;
+
         mSensor = sensorHelper.getCameraActivationSensor();
     }
 
@@ -58,7 +62,7 @@ public class CameraActivationSensor implements SensorEventListener, UpdatedState
     @Override
     public void onSensorChanged(SensorEvent event) {
         Log.d(TAG, "activate camera");
-        mCMActionsSettings.cameraAction();
+        mAction.action();
     }
 
     @Override
