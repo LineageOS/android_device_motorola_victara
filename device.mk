0897a579bcbca3dcccd184b506d6c3d10ac3df2b
Christopher R. Palmer <crpalmer@gmail.com>
2015-06-29 16:48:47 -0400

    victara: Remove reference to deprecated ffmpeg codecs.xml
    Change-Id: I4a45d04cd96b0d2f026026ffefae9a116df76ad5

--- device.mk
+++ device.mk
@@ -150,8 +150,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Motorola
 PRODUCT_PACKAGES += \
