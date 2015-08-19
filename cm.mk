d538acd96da86f3c0a3599919366bdd67651b1be
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-19 08:31:28 -0500

    init: set proper models and fingerprints for variants
    Change-Id: Ib813329157c564309e0369b415c1a5903590e0ca

--- cm.mk
+++ cm.mk
@@ -8,8 +8,3 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
 PRODUCT_NAME := cm_victara
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=victara_tmo \
-    BUILD_FINGERPRINT=motorola/victara_tmo/victara:5.0/LXE22.46-11/10:user/release-keys \
-    PRIVATE_BUILD_DESC="victara_tmo-user 5.0 LXE22.46-11 10 release-keys"
