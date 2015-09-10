4ce55d644d89b3a71b5c22df3cdff82382f67d5a
Dan Pasanen <dan.pasanen@gmail.com>
2015-09-09 19:33:11 -0500

    init: set ro.hw.radio on vzw variant
    * no idea why rmt_storage freaks out on the 32gb variants of
      xt1096 without this prop set but not having it causes chaos
    
    * set it and forget it
    
    Change-Id: I46d1a87c542eb6f1a54c0ed17d8024f119791989

--- init/init_victara.c
+++ init/init_victara.c
@@ -58,6 +58,7 @@ void vendor_load_properties()
         property_set("telephony.lteOnGsmDevice", "1");
     } else if (strstr(cid, "0x2")) {
         /* xt1096 */
+        property_set("ro.hw.radio", "0x4");
         property_set("ro.product.model", "XT1096");
         property_set("ro.build.description", "victara_verizon-user 5.1 LPE23.32-25-3 10 release-keys");
         property_set("ro.build.fingerprint", "motorola/victara_verizon/victara:5.1/LPE23.32-25-3/10:user/release-keys");
