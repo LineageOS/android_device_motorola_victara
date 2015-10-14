fcfe12be52012c64164638146afe0c8ea4b3ca84
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-13 23:54:24 -0500

    charge_only_mode: switch to libz
    * libunz has been deprecated
    
    Change-Id: I48ed3330cca9a87ade729e6e7a00a496bb8bb22a

--- charge_only_mode/Android.mk
+++ charge_only_mode/Android.mk
@@ -14,7 +14,7 @@ LOCAL_SRC_FILES += \
     draw.c \
     screen.c
 
-LOCAL_STATIC_LIBRARIES := libunz libcutils libc liblog
+LOCAL_STATIC_LIBRARIES := libz libcutils libc liblog
 
 LOCAL_C_INCLUDES := external/zlib
 
