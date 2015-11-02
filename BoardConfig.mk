f7a2226ad4a69a229130de23cca3efa695532508
myfluxi <linflux@arcor.de>
2015-11-02 07:50:18 -0600

    victara: remove deprecated bionic flag
    Change-Id: I540c605af4751808f8b2a077781310b6e4088014

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,6 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/motorola/victara/mkbootimg.mk
