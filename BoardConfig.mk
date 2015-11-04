f8cd46ef1fdcb968380cc39f000042a7740ad35d
Steve Kondik <steve@cyngn.com>
2015-11-04 14:52:46 -0800

    victara: Enable temporary workaround for gralloc issue
     * Remove when upstream code is ready.
    
    Change-Id: Icdefa5f49291844e62d1307a02534d6cd01b8770

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,7 @@ TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_USE_COMPAT_GRALLOC_PERFORM := true
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
