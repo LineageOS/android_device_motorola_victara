9ab887ee94f0be9d441a665fe6e576d858799c68
Dan Pasanen <dan.pasanen@gmail.com>
2015-12-07 15:06:33 -0600

    bdroid: sync with bacon
    Change-Id: I710085ea47c66d591c56e96b2b6c4ef9c43afe43

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project <http://www.cyanogenmod.org>
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,16 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME              "Moto X 2014"
-#define BLUETOOTH_QTI_SW                TRUE
+#define BLUETOOTH_QTI_SW TRUE
+
+#define BTM_DEF_LOCAL_NAME   "Moto X 2014"
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
-// Enables interleaved scan
-#define BTA_HOST_INTERLEAVE_SEARCH TRUE
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
 #endif
