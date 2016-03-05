69dd1fc45f64241808f40670d62decf02d648a78
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-05 06:57:49 -0500

    victara: We don't support low_latency properly, don't advertise it
    Change-Id: I9280cf058e412c12e8899d8b23ac8e7b0673e789

--- device.mk
+++ device.mk
@@ -20,7 +20,6 @@ $(call inherit-product, vendor/motorola/victara/victara-vendor.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
