81f38f13e2204afb4e9b3fb5437ef032a2ed4f55
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-05 10:35:30 -0500

    victara: init: Restore the normal hispeed load
    Change-Id: I9d4cd4685ca45840e91613e014d9d06c3b7237c0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 10970071040
 
 # Power HAL
 TARGET_POWERHAL_VARIANT := qcom
-TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/motorola/victara/power/power_ext.c
 
 # Qualcomm support
 
