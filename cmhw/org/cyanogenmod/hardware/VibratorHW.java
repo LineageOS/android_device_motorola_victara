7c8d0282ea96b992fe703a000443dbc02650e902
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-13 13:27:00 -0500

    cmhw: Use cmsdk FileUtils
    * remove unused import(s) as well
    
    Change-Id: I02a0f184aeba16ca87bc7eb252febf51ea7bf2c2

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -16,14 +16,13 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
-import java.io.File;
+import org.cyanogenmod.internal.util.FileUtils;
 
 public class VibratorHW {
     private static String LEVEL_PATH = "/sys/vibrator/pwmvalue";
 
     public static boolean isSupported() {
-        return new File(LEVEL_PATH).exists();
+        return FileUtils.isFileWritable(LEVEL_PATH);
     }
 
     public static int getMaxIntensity()  {
