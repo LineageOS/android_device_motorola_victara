cde634cddae13372e9d58f53eb53514cd49b1131
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-08-28 17:38:41 -0500

    init: update for N
    Change-Id: Iad834e99dc9659bf61356946e96340eb01252565

--- init/init_victara.cpp
+++ init/init_victara.cpp
@@ -36,38 +36,37 @@
 
 void vendor_load_properties()
 {
-    char platform[PROP_VALUE_MAX];
-    char cid[PROP_VALUE_MAX];
-    char radio[PROP_VALUE_MAX];
-    int rc;
+    std::string platform;
+    std::string cid;
+    std::string radio;
 
-    rc = property_get("ro.board.platform", platform);
-    if (!rc || strncmp(platform, ANDROID_TARGET, PROP_VALUE_MAX))
+    platform = property_get("ro.board.platform");
+    if (platform != ANDROID_TARGET)
         return;
 
-    if (property_get("ro.boot.radio", radio) > 0)
-        property_set("ro.hw.radio", radio);
+    radio = property_get("ro.boot.radio");
+    property_set("ro.hw.radio", radio.c_str());
 
-    property_get("ro.boot.cid", cid);
+    cid = property_get("ro.boot.cid");
 
     property_set("ro.build.product", "victara");
     property_set("ro.product.device", "victara");
 
-    if (strstr(cid, "0xE")) {
+    if (cid == "0xE") {
         /* xt1097 */
         property_set("ro.product.model", "XT1097");
         property_set("ro.build.description", "victara_retca-user 5.1 LPE23.32-48.1 1 release-keys");
         property_set("ro.build.fingerprint", " motorola/victara_retca/victara:5.1/LPE23.32-48.1/1:user/release-keys");
         property_set("ro.telephony.default_network", "9");
         property_set("telephony.lteOnGsmDevice", "1");
-    } else if (strstr(cid, "0x7")) {
+    } else if (cid == "0x7") {
         /* xt1092 */
         property_set("ro.product.model", "XT1092");
         property_set("ro.build.description", "victara_reteu-user 5.1 LPE23.32-25.1 1 release-keys");
         property_set("ro.build.fingerprint", " motorola/victara_reteu/victara:5.1/LPE23.32-25.1/1:user/release-keys");
         property_set("ro.telephony.default_network", "9");
         property_set("telephony.lteOnGsmDevice", "1");
-    } else if (strstr(cid, "0x2")) {
+    } else if (cid == "0x2") {
         /* xt1096 */
         property_set("ro.product.model", "XT1096");
         property_set("ro.build.description", "victara_verizon-user 5.1 LPE23.32-25-3 10 release-keys");
@@ -81,7 +80,7 @@ void vendor_load_properties()
         property_set("ro.com.google.clientidbase.ms", "android-verizon");
         property_set("ro.com.google.clientidbase.yt", "android-verizon");
         property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000");
-    } else if (strstr(cid, "0x9")) {
+    } else if (cid == "0x9") {
         /* xt1093 */
         property_set("ro.product.model", "XT1093");
         property_set("ro.build.description", "victara_usc-user 5.1 LPE23.32-21.7 1 release-keys");
