a9dc4dd7dbf53ee35658e02f549e2f26a497bda9
Gabriele M <moto.falcon.git@gmail.com>
2016-11-02 07:10:14 -0500

    cmactions: Hold a wakelock when the screen is on
    Change-Id: I497b09a22bc42bace26be072d49387d09ff45c75
    (cherry picked from commit 51811e6a6afbfe81c26de2491420e22d123fecd2)

--- cmactions/src/com/cyanogenmod/settings/device/CMActionsService.java
+++ cmactions/src/com/cyanogenmod/settings/device/CMActionsService.java
@@ -35,6 +35,7 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
     private final DozePulseAction mDozePulseAction;
     private final IrGestureManager mIrGestureManager;
     private final PowerManager mPowerManager;
+    private final PowerManager.WakeLock mWakeLock;
     private final ScreenReceiver mScreenReceiver;
     private final SensorHelper mSensorHelper;
 
@@ -73,6 +74,7 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
                 mIrGestureManager));
 
         mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "CMActionsWakeLock");
         updateState();
     }
 
@@ -82,6 +84,9 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
 
     @Override
     public void screenTurnedOn() {
+        if (!mWakeLock.isHeld()) {
+            mWakeLock.acquire();
+        }
         for (ScreenStateNotifier screenStateNotifier : mScreenStateNotifiers) {
             screenStateNotifier.screenTurnedOn();
         }
@@ -89,6 +94,9 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
 
     @Override
     public void screenTurnedOff() {
+        if (mWakeLock.isHeld()) {
+            mWakeLock.release();
+        }
         for (ScreenStateNotifier screenStateNotifier : mScreenStateNotifiers) {
             screenStateNotifier.screenTurnedOff();
         }
@@ -96,14 +104,8 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
 
     public void updateState() {
         if (mPowerManager.isInteractive()) {
-            if (!mWakeLock.isHeld()) {
-                mWakeLock.acquire();
-            }
             screenTurnedOn();
         } else {
-            if (mWakeLock.isHeld()) {
-                mWakeLock.release();
-            }
             screenTurnedOff();
         }
         for (UpdatedStateNotifier notifier : mUpdatedStateNotifiers) {
