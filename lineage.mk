a46b4b8c759965c3783ea33815718a9dfd604c65
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-02 13:18:21 -0600

    victara: cm->lineage
    Change-Id: Ie875ea9fa53a6402086267e96c82449e6f160907

--- lineage.mk
+++ lineage.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := cm_victara
+PRODUCT_NAME := lineage_victara
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
