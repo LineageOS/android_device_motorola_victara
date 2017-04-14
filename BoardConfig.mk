d8fb1dd828b41ab5e6b51f3c00409b881aa5ca69
Dan Pasanen <dan.pasanen@gmail.com>
2017-04-14 14:54:35 -0500

    victara: remove deprecated TARGET_INIT_VENDOR_LIB flag
    Change-Id: I46582dc90459476983418203fe7b25aa5f6ee353

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,7 +141,6 @@ BOARD_SEPOLICY_DIRS += device/motorola/victara/sepolicy
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_victara
 TARGET_RECOVERY_DEVICE_MODULES := libinit_victara
 
 # Vold
