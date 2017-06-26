fdb2bb8e18e0972cba729dfae17ce12bb9fcc938
Bruno Martins <bgcngm@gmail.com>
2017-06-26 16:09:38 -0500

    victara: Abandon TARGET_UNIFIED_DEVICE flag
    * Override the unified properties using vendor init
    
    Change-Id: I01cd260d0dec039005dfaf794be5536e992010fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,7 +140,6 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/motorola/victara/sepolicy
 
 # Vendor Init
-TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_victara
 TARGET_RECOVERY_DEVICE_MODULES := libinit_victara
 
