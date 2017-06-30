fe968b938e1fa527c99c1d2d8f8db24924b1e741
Mikhail Naganov <mnaganov@google.com>
2017-06-30 09:59:19 -0700

    victara: Remove Broadcast Radio HAL impl library
    There is no support for FM radio on this line of devices.
    Having this HAL impl in place was confusing VTS tests runner
    as broadcast radio HAL was present in the output of 'lshal'.
    
    Bug: 33050893
    Test: audio works, no impl lib in /vendor/lib[64]/hw,
          no broadcastradio in lshal output
    
    Change-Id: I35b4d6329de8b04032b456fb048b0b8728024a64

--- device.mk
+++ device.mk
@@ -30,7 +30,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio_policy.msm8974 \
