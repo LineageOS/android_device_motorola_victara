e703785980cb8324affd5b2e689875c97acd1457
AdrianDC <radian.dc@gmail.com>
2017-08-18 04:20:31 -0500

    camera: Add libbase static library
    * Required to access android-base/unique_fd.h

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,6 +9,9 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
+LOCAL_STATIC_LIBRARIES := \
+    libbase
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
