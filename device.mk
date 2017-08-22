f4db3774ed0c54c1b2aaa11584ccc5570e79549c
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:18:33 +0200

    victara: Build USB HAL
    Change-Id: I9d2b27305109b691a197d50afe3044233fb4504b

--- device.mk
+++ device.mk
@@ -267,6 +267,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Torch
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
