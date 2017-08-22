c6bd4e88509adeb6abbda2d23c2d7629565ed741
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:10:19 +0200

    victara: Build DRM HAL
    Change-Id: Iaf4aae990d243423c0be41f0e629c936938e8982

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_PACKAGES += \
     regdbdump \
     regulatory.bin
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
