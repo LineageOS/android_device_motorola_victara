94c630900d865d91ffc27dad986bd1ddb35659ae
Artem Borisov <dedsa2002@gmail.com>
2017-08-26 13:24:42 +0300

    victara: libshims: Bring back atomic calls for mpdecision
    Change-Id: Ifa857868a75fe0414b63ee2b55c290a3e75f3646

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -27,3 +27,13 @@ LOCAL_SHARED_LIBRARIES := libutils libgui liblog libbinder libsensor
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := atomic.cpp
+
+LOCAL_MODULE := libshims_atomic
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := liblog
+
+include $(BUILD_SHARED_LIBRARY)
