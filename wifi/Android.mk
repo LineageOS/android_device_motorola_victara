3eb28dbce455cb8527c740a1802b5ae0f18fbf31
Luca Stefani <luca020400@lineageos.org>
2017-08-27 20:32:05 +0200

    victara: Kill unused hostapd configs
    Change-Id: If438a360b54ac04d2b2cfd4928e2256a3837a937

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -5,14 +5,6 @@ LOCAL_PATH:= $(call my-dir)
 #----------------------------------------------------------------------
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := hostapd_default.conf
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
-LOCAL_SRC_FILES    := hostapd.conf
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd.accept
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
