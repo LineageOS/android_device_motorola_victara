b875f3f9861eb560888766addc0fd6100fc8cea1
LuK1337 <priv.luk@gmail.com>
2017-09-03 00:55:22 +0200

    victara: wifi: Copy WCNSS_qcom_cfg.ini to vendor partition
    * Required for O.
    
    Change-Id: I4426e5a9a8338d6ea584b506f4e5e23bcbf97d81

--- device.mk
+++ device.mk
@@ -298,7 +298,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
