6c93c68ea6e0acb9b4cb5e41bc4e7d02b373ad13
Alberto97 <albertop2197@gmail.com>
2017-09-05 13:28:34 +0200

    victara: LineageActions: Integrate with Settings, again
    * Use O theme
     * Revert back to PreferenceActivity since drawer is gone
     * Reintroduce back navigation
    
    Change-Id: Ib07fcfea6c4fc8a2e272f2534c16894973311337
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- LineageActions/src/com/lineageos/settings/device/TouchscreenGesturePreferenceActivity.java
+++ LineageActions/src/com/lineageos/settings/device/TouchscreenGesturePreferenceActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 The CyanogenMod Project
+ * Copyright (c) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +18,7 @@
 package com.lineageos.settings.device;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
 import androidx.fragment.app.FragmentActivity;
 
--- LineageActions/src/com/lineageos/settings/device/TouchscreenGesturePreferenceFragment.java
+++ LineageActions/src/com/lineageos/settings/device/TouchscreenGesturePreferenceFragment.java
@@ -16,7 +16,9 @@
 
 package com.lineageos.settings.device;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.MenuItem;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
 
@@ -26,10 +28,21 @@ public class TouchscreenGesturePreferenceFragment extends PreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.gesture_panel);
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         PreferenceCategory ambientDisplayCat = (PreferenceCategory)
                 findPreference(CATEGORY_AMBIENT_DISPLAY);
         if (ambientDisplayCat != null) {
             ambientDisplayCat.setEnabled(LineageActionsSettings.isDozeEnabled(getActivity().getContentResolver()));
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
     }
 }
