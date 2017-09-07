fa8c398bff472b339767c604f3c780123288fbc7
LuK1337 <priv.luk@gmail.com>
2017-09-07 16:19:19 +0200

    victara: libshims: Add M sensorlistener shim for libmot_sensorlistener.so
    Change-Id: I7660b9dc3a0e929c1f6735448e3242d913e15afe

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -37,3 +37,26 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := liblog
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    sensorlistener/ISensorServer.cpp \
+    sensorlistener/SensorManager.cpp
+
+LOCAL_MODULE := libshims_sensorlistener
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := \
+    libnativeloader \
+    libbinder \
+    libcutils \
+    libEGL \
+    libGLESv2 \
+    libsync \
+    libui \
+    libutils \
+    liblog \
+    libbase \
+    libsensor
+
+include $(BUILD_SHARED_LIBRARY)
