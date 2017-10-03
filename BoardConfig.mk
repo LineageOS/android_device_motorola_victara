ae875fa6e1948c675115c7be7a171549d9e46763
Scott Warner <Tortel1210@gmail.com>
2017-10-03 11:48:49 -0400

    victara: Switch to selinux permissive
    Change-Id: I2d70cf0b7494bedcf1f5d408a6b502485f1830c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ TARGET_CPU_VARIANT := krait
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=400M
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_LZ4C_DT := true
 BOARD_KERNEL_PAGESIZE := 2048
