22ae8870c88ed382a8ad3263e2db089304aadba2
Ritesh Harjani <riteshh@codeaurora.org>
2017-11-16 00:30:10 -0500

    victara: Add AID_QCOM_DIAG, AID_RFS, and AID_RFS_SHARED to config.fs
    Change-Id: I9da576900ed29a2633bb997dbbea4565b489a418

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@ TARGET_FS_CONFIG_GEN := \
     $(LOCAL_PATH)/fs_config/mot_aids.txt \
     $(LOCAL_PATH)/fs_config/file_caps.txt
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8974
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
