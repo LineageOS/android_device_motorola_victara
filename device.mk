abba2c9d70ae78bf29e07f66e68f04b7ddf4d3d2
Paul Keith <javelinanddart@gmail.com>
2017-12-30 23:56:33 +0100

    victara: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: I2abf4ca9b0b5a0cb031f12c400a1159962f5e94c

--- device.mk
+++ device.mk
@@ -226,8 +226,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8974
+    android.hardware.power@1.0-service-qti
 
 # Ramdisk
 PRODUCT_PACKAGES += \
