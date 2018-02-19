8921e5c95ef83da1a4072a32b2d661cba00c31ac
Luca Stefani <luca020400@lineageos.org>
2018-02-19 12:39:43 -0500

    victara: Build camera HAL
    Change-Id: If2f1a3954fa1b70d49deff481a8116d20be7b012

--- device.mk
+++ device.mk
@@ -122,6 +122,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
     Snap \
     libshim_camera \
     camera.msm8974
