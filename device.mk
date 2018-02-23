904e81be87a424503a07b68c0d5c416aae9222b4
Bruno Martins <bgcngm@gmail.com>
2018-02-23 10:21:34 +0000

    victara: Remove wificond from device makefiles
    * This package is being built globally.
    * Also, at some point we apparently duplicated it, so
      remove that as well.
    
    Change-Id: I70aaf469acc29ded3cf5bb24ab112a76b36a7835

--- device.mk
+++ device.mk
@@ -340,11 +340,9 @@ PRODUCT_PACKAGES += \
     hostapd.deny \
     libqsap_sdk \
     libwpa_client \
-    wificond \
     wcnss_service \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    wificond
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
