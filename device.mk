d54159ce6119988cda68fd95d74d051e32fb26a3
Scott Warner <Tortel1210@gmail.com>
2018-06-05 12:45:46 -0400

    victara: Fix audio config paths
    Change-Id: Iea3bfb10fc07bcdd14213337c383f3ff46ae8a0f

--- device.mk
+++ device.mk
@@ -116,9 +116,9 @@ PRODUCT_PACKAGES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml
 
 # Camera
 PRODUCT_PACKAGES += \
