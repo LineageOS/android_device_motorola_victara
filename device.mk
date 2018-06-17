9ab6c14f24a3438ee4b2d714c8394ba34db1c0ed
Scott Warner <Tortel1210@gmail.com>
2018-06-17 10:53:05 -0400

    victara: Convert audio_effects.conf to xml
    Converted using luk1337's aeffects-conf2xml tool:
    https://github.com/luk1337/aeffects-conf2xml
    
    Change-Id: I0de6368fd36445d2d53bfc2bc2c8221c4293cfc4

--- device.mk
+++ device.mk
@@ -115,7 +115,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml
