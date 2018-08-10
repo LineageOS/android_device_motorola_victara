8df64f5a93ad77c263b0faaa8b67d7c8cfe1de94
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:14:57 +0100

    victara: Import stock Dalvik heap overrides
    Change-Id: Ifac6650a874cf982a6e767da92599d4b6230a637

--- device.mk
+++ device.mk
@@ -86,7 +86,14 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+# Dalvik heap
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
 
 # Art
 PRODUCT_PROPERTY_OVERRIDES += \
