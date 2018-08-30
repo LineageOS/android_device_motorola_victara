a5dba15c1822c2292c6909e1966f39f4c33536bf
jrior001 <jriordan001@gmail.com>
2018-08-29 22:51:19 -0400

    nfc: libnfc-brcm.conf -> libnfc-nci.conf
    Change-Id: Ib234f09778166848d66de6ba0f1d5237d839de0c

--- device.mk
+++ device.mk
@@ -224,8 +224,8 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
 PRODUCT_COPY_FILES += \
-    device/motorola/victara/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
-    device/motorola/victara/nfc/libnfc-brcm-20795a10.conf:system/vendor/etc/libnfc-brcm-20795a10.conf
+    device/motorola/victara/nfc/libnfc-nci.conf:system/vendor/etc/libnfc-nci.conf \
+    device/motorola/victara/nfc/libnfc-nci-20795a10.conf:system/vendor/etc/libnfc-nci-20795a10.conf
 
 # OMX
 PRODUCT_PACKAGES += \
