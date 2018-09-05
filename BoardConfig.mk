1eb9eafaa91b728656e23a7399676e83b05a4760
Bruno Martins <bgcngm@gmail.com>
2018-09-05 23:01:52 +0100

    victara: Only include legacy QC sepolicy
     * This has now turned into a separate repository (maintained only
       for legacy devices, those that never got official Orea updates).
    
    Change-Id: I981b452b697bc3610d7aa97b74ed182c6b70ca30

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,8 +165,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_USE_SDCLANG := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += device/motorola/victara/sepolicy
 
