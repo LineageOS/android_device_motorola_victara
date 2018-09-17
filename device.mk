a0ed83a383b4ddd21fa4acfd2b32a21adc389213
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    victara: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- device.mk
+++ device.mk
@@ -236,7 +236,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Ramdisk
 PRODUCT_PACKAGES += \
