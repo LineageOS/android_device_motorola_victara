7de595b2b9ef0ec6219dc7ef8d41dd4c610a37c2
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    victara: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
     * To do this we need to label /fsg as well
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- fs_config/config.fs
+++ fs_config/config.fs
@@ -6,3 +6,21 @@ value:2951
 
 [AID_VENDOR_RFS_SHARED]
 value:2952
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[fsg/]
+mode: 0755
+user: AID_ROOT
+group: AID_ROOT
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
