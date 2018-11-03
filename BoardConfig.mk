58e1a077a8c37509cebbb92e9057cdcf5bcab86c
Scott Warner <Tortel1210@gmail.com>
2018-11-03 07:42:10 -0400

    victara: Include qcom-legacy sepolicy
    Change-Id: Ib15e66fd3062514d6b5fbc2d0be4efd0a6ad8221

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,8 @@ TARGET_USE_SDCLANG := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
+
 BOARD_SEPOLICY_DIRS += device/motorola/victara/sepolicy
 
 # Vendor Init
