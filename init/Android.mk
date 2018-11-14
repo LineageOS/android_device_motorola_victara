3c9cdc9d889b53337b89425df007b89d648a9866
Scott Warner <Tortel1210@gmail.com>
2018-11-14 14:41:29 -0500

    victara: More init updates
    Set both vendor and system versions of some properties, bring in line with O vendor inits
    
    Change-Id: I44a0d1624e7f5b33af75161a1a136a53bc78a8e7

--- init/Android.mk
+++ init/Android.mk
@@ -3,11 +3,16 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_victara.cpp
 LOCAL_MODULE := libinit_victara
-LOCAL_STATIC_LIBRARIES := \
-    libbase
+
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init \
+    external/selinux/libselinux/include \
+    external/libcap/libcap/include
+
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
