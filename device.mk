24c097fceb56464b9689bdab44ec1de2b0e7dc35
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    victara: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>

--- device.mk
+++ device.mk
@@ -319,6 +319,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Torch
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
