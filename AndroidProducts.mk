34580c1098c4d0a047e730cd69fd28b7479cb47f
Scott Warner <Tortel1210@gmail.com>
2019-01-20 19:47:27 -0500

    victara: Clean up makefiles
    Why was there a full_victara.mk and lineage.mk? Combine them
    
    Change-Id: I611596fcdeb7984c3c090847557867c462fcd22d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_victara.mk
+    $(LOCAL_DIR)/lineage_victara.mk
---
+++ lineage_victara.mk
@@ -0,0 +1,21 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from victara device
+$(call inherit-product, device/motorola/victara/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := victara
+PRODUCT_NAME := lineage_victara
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := victara
+PRODUCT_RELEASE_NAME := MOTO X (2014)
+PRODUCT_MANUFACTURER := motorola
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
