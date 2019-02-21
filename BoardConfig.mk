782e30ceeb2bbf1096ec8edc9395fdfd6a08f369
Anton Hansson <hansson@google.com>
2019-02-21 15:04:20 +0000

    victara: Set cpu "runtime variant" to krait
    This makes bionic and various art tools that compile/runs cpu-specific
    code choose the kryo variant at runtime. It does not affect
    build-time compilation.
    
    Bug: 120773446
    Test: bionic benchmarks with and without this change
    Change-Id: I670c8acd094090135515688c5f057c2cc08a083f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := krait
 
 # Binder API version
 TARGET_USES_64_BIT_BINDER := true
