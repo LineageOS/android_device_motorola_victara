c102269bdb731852c22828d95c822b2b6ba478f1
Michael Bestas <mkbestas@lineageos.org>
2019-08-06 19:31:49 +0000

    victara: Inherit common_mini_phone
    * We don't have enough system space to fit full GMS
      packages anymore so use the mini target while
      still building Recorder. This excludes about 25MB of
      LatinIME dictionaries.
    
    Change-Id: I418f1729df20da16a184d02e7e15c64df1053374

--- lineage_victara.mk
+++ lineage_victara.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 
 # Inherit from victara device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
