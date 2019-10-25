24f067083db82ff04f13a7f0084c761d0830a778
Elektroschmock <elektroschmock78@googlemail.com>
2019-10-25 22:32:34 +0200

    victara: Use legacy Wi-Fi service
    Change-Id: Ie264faa4740019f5cb4081e3b861ac9f3778923a

--- device.mk
+++ device.mk
@@ -314,7 +314,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     hostapd.accept \
     hostapd.deny \
