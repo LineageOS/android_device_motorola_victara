d0ab8f95994b682409b808265d1ea54708938d1e
Elektroschmock <elektroschmock78@googlemail.com>
2019-10-27 13:46:54 +0100

    victara: Build Clearkey 1.2 plugin and update vintf
    Also remove the widevine plugin we don't have
    
    Reference:
    https://github.com/LineageOS/android_hardware_interfaces/blob/lineage-17.0/drm/1.1/README.md
    
    Change-Id: Ie4ea8d79496ba6181ef6c45b0ce0a993a3e442c4

--- device.mk
+++ device.mk
@@ -156,7 +156,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # Display
 PRODUCT_PACKAGES += \
