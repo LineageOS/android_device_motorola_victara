ce513b8d91978551b2aa313748af3392a70ce18e
bgcngm <bgcngm@gmail.com>
2019-10-27 18:43:19 +0000

    victara: Drop mediaextractor seccomp policy
    * These syscalls are now allowed by default.
    
    * [arco] Also clean mediacodec.policy
    
    Change-Id: I21592135dfba78886ea17d7c95bac17778d60b59

--- device.mk
+++ device.mk
@@ -282,8 +282,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
