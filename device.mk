9e683fddce163597f4e599c6349d61429042ffb5
Aleksander <johnnylittleplanet@gmail.com>
2020-04-28 17:49:52 +0200

    victara: Binderize Keymaster HIDL
    Change-Id: If1003d6b128a8d37fd6a845b307f45af946a8adf

--- device.mk
+++ device.mk
@@ -151,6 +151,7 @@ PRODUCT_COPY_FILES += \
 # Keystore
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
     keystore.msm8974
 
 # Languages
