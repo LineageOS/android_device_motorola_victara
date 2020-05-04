633da8002999f323f37b6ef50cf0e98655d9a2e2
Nolen Johnson <johnsonnolen@gmail.com>
2020-05-04 11:37:36 -0400

    [DNM] victara: LineageActions: JNI debugging
    Change-Id: I1fcb932a760ba0fee671a440dda7a00be62431e1

--- lineageactions/Android.mk
+++ lineageactions/Android.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
