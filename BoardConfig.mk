4f66a4d66272c8d1d014e4d2219b4125b8252647
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-05-09 18:01:04 +0000

    victara: Clean deprecated LZ4C_DT flag
    Change-Id: I1dfdb81479cb27f9cdacb40d810f0e3005b11798

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,6 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=400M
 BOARD_KERNEL_IMAGE_NAME := zImage
-BOARD_KERNEL_LZ4C_DT := true
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
