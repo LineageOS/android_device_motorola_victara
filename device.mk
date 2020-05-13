a153e8b5fa7f344f1933f5997eb77e1188719d86
Nolen Johnson <johnsonnolen@gmail.com>
2020-05-13 10:01:16 -0400

    victara: Build some additional HIDLs
    * Rename memtrack HIDL appropriately.
    
    Change-Id: I2e39bac130d8ad94d8fdb0c794260adcc7459352

--- device.mk
+++ device.mk
@@ -95,9 +95,10 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-impl \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
