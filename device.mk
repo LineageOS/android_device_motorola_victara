1ad88d1336945b12a8053488a0ca4ff51eb1d15d
Arne Coucheron <arco68@gmail.com>
2020-05-18 06:29:20 +0200

    victara: gps: Rename for victara and label it for sepolicy
    Change-Id: I0d71f1a32d82a9a24eea0e3dffe7c8b463d17d9d

--- device.mk
+++ device.mk
@@ -113,8 +113,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service \
+    android.hardware.gnss@1.0-impl.victara \
+    android.hardware.gnss@1.0-service.victara \
     gps.msm8974
 
 # Health
