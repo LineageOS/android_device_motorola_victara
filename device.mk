35e34e2d757f4c7543d8af3359fb1b8bac4367b3
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    victara: Add prebuilt protobuf from sdk29
    This is needed for our prebuilt DRM library.
    
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- device.mk
+++ device.mk
@@ -339,6 +339,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service.legacy \
