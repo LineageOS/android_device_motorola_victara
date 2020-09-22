e1f4b7ca7214073fddf3d347824445063e117415
LuK1337 <priv.luk@gmail.com>
2020-09-22 10:15:09 +0200

    victara: Enforce vintf manifest override
    * Apparently this is now required on all non full treble devices since
      google added a sleep(1) in libhidl...
    
    Change-Id: I6d8492dd7c56bfdb2ca00f3477634a46fa55b1c0

--- device.mk
+++ device.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
