e77aaf71e9f92dd12575e0a1db92e61cf5e8609a
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    victara: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- device.mk
+++ device.mk
@@ -282,7 +282,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # RenderScript
 PRODUCT_PACKAGES += \
