8419fdfb0e87fbec0dbeab18ec642fd16c5310bf
chrmhoffmann <chrmhoffmann@gmail.com>
2020-12-11 16:14:38 +0100

    victara: Add vibrator HAL and enable different vibration strengths
    * Vibrator HAL code kanged from Shamu with slight modifications
    
    Change-Id: Id502737cc377a7d3ca12840feecf0861cef70f28

--- device.mk
+++ device.mk
@@ -346,8 +346,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.1-service.victara
 
 # VNDK
 PRODUCT_COPY_FILES += \
