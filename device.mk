28b61f0f7700f07cfe0afe320caf3447c16dc235
Andrea <linckandrea01@gmail.com>
2021-02-17 17:59:06 +0100

    victara: binderize BT
    Change-Id: I8808ae235cf985cf85e8fadb619b9fb1eeb2acaa

--- device.mk
+++ device.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Boot animation
