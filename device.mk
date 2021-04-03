3e899c5deadd53a6e444b0e3a9adfecabd630963
Andrea <linckandrea01@gmail.com>
2021-04-03 21:33:10 +0200

    victara: device: build clearkey 1.3 plugin
     * fix also relative sepolicy denials
    
     * this partially revert @Ie4ea8d79496ba6181ef6c45b0ce0a993a3e442c4
    
     * reason: on R it is broken (on our device)
    
    Change-Id: Ic5adbe8c759e512b13d2610d7c22354d87595c24

--- device.mk
+++ device.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
