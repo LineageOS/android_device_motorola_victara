d425cac71e0677f776074002f1a8bec90ab8c229
Jeferson Oliveira <jroliveira.oliveira301@gmail.com>
2021-04-13 12:44:26 +0200

    victara: gps: Partilaly revert inclusion of  oppo GPS HAL
    * Keep the local copy of configuration files
    
    This partially reverts commit 6b573fb.
    
    Change-Id: I5a7b6743ebce885ad4a49d5e5b933ddcd4dfa911

--- gps/utils/loc_log.cpp
+++ gps/utils/loc_log.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, 2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -51,15 +51,14 @@ const char FROM_MODEM[] = "<---";
 const char TO_AFW[]     = "<===";
 const char EXIT_TAG[]   = "Exiting";
 const char ENTRY_TAG[]  = "Entering";
-const char EXIT_ERROR_TAG[]  = "Exiting with error";
 
 /* Logging Mechanism */
 loc_logger_s_type loc_logger;
 
 /* Get names from value */
-const char* loc_get_name_from_mask(const loc_name_val_s_type table[], size_t table_size, long mask)
+const char* loc_get_name_from_mask(loc_name_val_s_type table[], int table_size, long mask)
 {
-   size_t i;
+   int i;
    for (i = 0; i < table_size; i++)
    {
       if (table[i].val & (long) mask)
@@ -71,9 +70,9 @@ const char* loc_get_name_from_mask(const loc_name_val_s_type table[], size_t tab
 }
 
 /* Get names from value */
-const char* loc_get_name_from_val(const loc_name_val_s_type table[], size_t table_size, long value)
+const char* loc_get_name_from_val(loc_name_val_s_type table[], int table_size, long value)
 {
-   size_t i;
+   int i;
    for (i = 0; i < table_size; i++)
    {
       if (table[i].val == (long) value)
@@ -84,7 +83,7 @@ const char* loc_get_name_from_val(const loc_name_val_s_type table[], size_t tabl
    return UNKNOWN_STR;
 }
 
-static const loc_name_val_s_type loc_msg_q_status[] =
+static loc_name_val_s_type loc_msg_q_status[] =
 {
     NAME_VAL( eMSG_Q_SUCCESS ),
     NAME_VAL( eMSG_Q_FAILURE_GENERAL ),
@@ -93,7 +92,7 @@ static const loc_name_val_s_type loc_msg_q_status[] =
     NAME_VAL( eMSG_Q_UNAVAILABLE_RESOURCE ),
     NAME_VAL( eMSG_Q_INSUFFICIENT_BUFFER )
 };
-static const size_t loc_msg_q_status_num = LOC_TABLE_SIZE(loc_msg_q_status);
+static int loc_msg_q_status_num = sizeof(loc_msg_q_status) / sizeof(loc_name_val_s_type);
 
 /* Find msg_q status name */
 const char* loc_get_msg_q_status(int status)
@@ -107,18 +106,17 @@ const char* log_succ_fail_string(int is_succ)
 }
 
 //Target names
-static const loc_name_val_s_type target_name[] =
+loc_name_val_s_type target_name[] =
 {
     NAME_VAL(GNSS_NONE),
     NAME_VAL(GNSS_MSM),
     NAME_VAL(GNSS_GSS),
     NAME_VAL(GNSS_MDM),
     NAME_VAL(GNSS_QCA1530),
-    NAME_VAL(GNSS_AUTO),
     NAME_VAL(GNSS_UNKNOWN)
 };
 
-static const size_t target_name_num = LOC_TABLE_SIZE(target_name);
+static int target_name_num = sizeof(target_name)/sizeof(loc_name_val_s_type);
 
 /*===========================================================================
 
@@ -139,7 +137,7 @@ const char *loc_get_target_name(unsigned int target)
     static char ret[BUFFER_SIZE];
 
     index =  getTargetGnssType(target);
-    if( index < 0 || (unsigned)index >= target_name_num )
+    if( index >= target_name_num || index < 0)
         index = target_name_num - 1;
 
     if( (target & HAS_SSC) == HAS_SSC ) {
@@ -168,7 +166,7 @@ RETURN VALUE
    The time string
 
 ===========================================================================*/
-char *loc_get_time(char *time_string, size_t buf_size)
+char *loc_get_time(char *time_string, unsigned long buf_size)
 {
    struct timeval now;     /* sec and usec     */
    struct tm now_tm;       /* broken-down time */
@@ -202,12 +200,6 @@ SIDE EFFECTS
 void loc_logger_init(unsigned long debug, unsigned long timestamp)
 {
    loc_logger.DEBUG_LEVEL = debug;
-#ifdef TARGET_BUILD_VARIANT_USER
-   // force user builds to 2 or less
-   if (loc_logger.DEBUG_LEVEL > 2) {
-       loc_logger.DEBUG_LEVEL = 2;
-   }
-#endif
    loc_logger.TIMESTAMP   = timestamp;
 }
 
