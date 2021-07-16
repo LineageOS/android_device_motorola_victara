421036b053e998255f57110a0881101748831785
Nolen Johnson <johnsonnolen@gmail.com>
2021-07-16 19:06:20 +0200

    Revert "victara: Add vibrator HAL and enable different vibration strengths"
    This reverts commit 6b55a2395b6bd97151e56b1808a3fad64a4a6713.
    
    Reason for revert: This broke specific haptics cases.
    
    Change-Id: Iba7a4e22672d5ad31adde98d178bef15289f092d

--- device.mk
+++ device.mk
@@ -346,7 +346,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.1-service.victara
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # VNDK
 PRODUCT_COPY_FILES += \
